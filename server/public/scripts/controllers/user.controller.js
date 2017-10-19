ad1c8dd64907a7e4c2fe7afee66171d15bb86f99
Carla Brady <carlakaybrady@gmail.com>
2017-10-19 11:30:45 -0500

    styled game cards

--- server/public/scripts/controllers/user.controller.js
+++ server/public/scripts/controllers/user.controller.js
@@ -4,13 +4,25 @@ myApp.controller('UserController', function(UserService) {
     vm.userService = UserService;
     vm.userObject = UserService.userObject;
 
+    function makeTitle(str) { //not working
+      if (typeof str !== 'string') {
+          throw new TypeError ('expects a string');
+      } else {
+        return str.split(' ').map(function(word) {
+            return word[0].toUpperCase() + word.substr(1);
+        }).join(' ');
+      };
+    };
+
     vm.getGame = function (search) {
+      makeTitle(search);     //attempting to capitalize search
+      console.log('make title search:', search);
       UserService.getGame(search).then( function () {
+        console.log('title search in get game:', search);        
         vm.games = UserService.userObject.games;
         console.log('game return in controller', UserService.userObject.games );
         console.log('vm.games:', vm.games);
       })
     };
-    
     
 });
